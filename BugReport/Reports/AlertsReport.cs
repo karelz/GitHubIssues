4f705d71f83165f0e432af492cbecc24e00ef5c3
Karel Zikmund <karelz@microsoft.com>
2016-10-25 18:15:54 -0700

    Enable sending emails

--- BugReport/Reports/AlertsReport.cs
+++ BugReport/Reports/AlertsReport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -21,6 +22,9 @@ namespace BugReport.Reports
 
         public void SendEmails(IssueCollection issuesStart, IssueCollection issuesEnd)
         {
+            SmtpClient smtpClient = new SmtpClient("smtphost");
+            smtpClient.UseDefaultCredentials = true;
+
             foreach (Alert alert in Alerts)
             {
                 alert.Query.Validate(issuesEnd);
@@ -29,6 +33,59 @@ namespace BugReport.Reports
 
                 IEnumerable<Issue> onlyStart = queryStart.Except(queryEnd);
                 IEnumerable<Issue> onlyEnd = queryEnd.Except(queryStart);
+
+                if (!onlyStart.Any() && !onlyEnd.Any())
+                {
+                    continue;
+                }
+
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(Environment.UserName + "@microsoft.com");
+                foreach (Alert.User user in alert.Owners)
+                {
+                    message.To.Add(user.EmailAddress);
+                }
+                foreach (Alert.User user in alert.CCs)
+                {
+                    message.To.Add(user.EmailAddress);
+                }
+                message.Subject = "Test email from my tool";
+                message.IsBodyHtml = true;
+
+                StringBuilder text = new StringBuilder("Hello");
+                text.AppendLine("<br/>");
+                if (onlyStart.Any())
+                {
+                    text.AppendLine("  only start:");
+                    text.AppendLine("<br/>");
+                    foreach (Issue issue in onlyStart)
+                    {
+                        text.AppendFormat("    #{0} - {1}", issue.Number, issue.Title);
+                        text.AppendLine("<br/>");
+                    }
+                }
+                if (onlyEnd.Any())
+                {
+                    text.AppendLine("  only end:");
+                    text.AppendLine("<br/>");
+                    foreach (Issue issue in onlyEnd)
+                    {
+                        text.AppendFormat("    #{0} - {1}", issue.Number, issue.Title);
+                        text.AppendLine("<br/>");
+                    }
+                }
+
+                message.Body = text.ToString();
+
+                try
+                {
+                    smtpClient.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR sending alert {0}", alert.Name);
+                    Console.WriteLine(ex);
+                }
 
                 Console.WriteLine("Alert {0}", alert.Name);
                 Console.WriteLine("    Onwers:");
@@ -41,22 +98,7 @@ namespace BugReport.Reports
                 {
                     Console.WriteLine("        {0} - {1}", user.Name, user.EmailAddress);
                 }
-                if (onlyStart.Any())
-                {
-                    Console.WriteLine("  only start:");
-                    foreach (Issue issue in onlyStart)
-                    {
-                        Console.WriteLine("    #{0} - {1}", issue.Number, issue.Title);
-                    }
-                }
-                if (onlyEnd.Any())
-                {
-                    Console.WriteLine("  only end:");
-                    foreach (Issue issue in onlyEnd)
-                    {
-                        Console.WriteLine("    #{0} - {1}", issue.Number, issue.Title);
-                    }
-                }
+                Console.Write(text.Replace("<br/>", ""));
             }
         }
 
