eddda1f9ff1b7ab8273a1295a6d33af3f984059d
Karel Zikmund <karelz@microsoft.com>
2017-03-12 03:24:36 -0700

    Missed files (again)

---
+++ BugReport/Reports/GitHubQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BugReport.DataModel;
+
+namespace BugReport.Reports
+{
+    public class GitHubQuery
+    {
+        private static string BaseUrl = "https://github.com/dotnet/corefx/issues?q=";
+
+        public static void GetHyperLink(StringBuilder sb, IEnumerable<DataModelIssue> issues)
+        {
+            sb.Append(BaseUrl);
+            foreach (DataModelIssue i in issues)
+            {
+                sb.AppendFormat("{0}%20", i.Number);
+            }
+        }
+        public static string GetHyperLink(IEnumerable<DataModelIssue> issues)
+        {
+            StringBuilder link = new StringBuilder();
+            GetHyperLink(link, issues);
+            return link.ToString();
+        }
+        public static string GetHyperLink(string query)
+        {
+            return BaseUrl + System.Net.WebUtility.UrlEncode(query);
+        }
+    }
+}
---
+++ BugReport/Reports/IssueEntry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using BugReport.Query;
+using BugReport.DataModel;
+
+namespace BugReport.Reports
+{
+    public struct IssueEntry
+    {
+        public string IssueId;
+        public string Title;
+        public string LabelsText;
+        public string AssignedToText;
+        public string MilestoneText;
+
+        // assignedToOverride - used for using 'Closed' as AssignedTo value in reports
+        public IssueEntry(DataModelIssue issue, string assignedToOverride = null)
+        {
+            string idPrefix = "";
+            if (issue.IsPullRequest)
+            {
+                idPrefix = "PR ";
+            }
+
+            IssueId = $"{idPrefix}#<a href=\"{issue.HtmlUrl}\">{issue.Number}</a>";
+
+            Title = issue.Title;
+
+            LabelsText = string.Join(", ", issue.Labels.Select(l => l.Name));
+
+            if (assignedToOverride != null)
+            {
+                AssignedToText = assignedToOverride;
+            }
+            else if (issue.Assignee != null)
+            {
+                AssignedToText = $"<a href=\"{issue.Assignee.HtmlUrl}\">@{issue.Assignee.Login}</a>";
+            }
+            else
+            {
+                AssignedToText = "";
+            }
+
+            if (issue.Milestone != null)
+            {
+                MilestoneText = issue.Milestone.Title;
+            }
+            else
+            {
+                MilestoneText = "";
+            }
+        }
+    }
+}
