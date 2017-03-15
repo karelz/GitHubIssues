70d4e6165c2e6fdbea5bdd6572b9a8193d9f9006
Karel Zikmund <karelz@microsoft.com>
2017-03-14 18:33:47 -0700

    Enable / in label names

--- BugReport/Query/Tokenizer.cs
+++ BugReport/Query/Tokenizer.cs
@@ -160,7 +160,8 @@ namespace BugReport.Query
             return (char.IsLetterOrDigit((char)c) ||
                     (c == '-') ||
                     (c == '_') ||
-                    (c == '.'));
+                    (c == '.') ||
+                    (c == '/'));
         }
 
         void SkipWhitespaces()
