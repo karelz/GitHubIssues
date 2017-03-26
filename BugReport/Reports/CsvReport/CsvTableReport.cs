1d03a9f203b9d72a6a901988622504785345411e
Karel Zikmund <karelz@microsoft.com>
2017-03-25 23:45:57 -0700

    Add CSV report - /out_csv option Add report 'name' optional argument into HTML and CSV reports

---
+++ BugReport/Reports/CsvReport/CsvTableReport.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using BugReport.Util;
+using BugReport.DataModel;
+using BugReport.Query;
+
+namespace BugReport.Reports
+{
+    public class CsvTableReport
+    {
+        private TableReport _report;
+        private string _reportName;
+
+        private CsvTableReport(TableReport report, string reportName)
+        {
+            _report = report;
+            _reportName = reportName;
+        }
+
+        public static void Write(TableReport report, string csvFileNamePrefix, string reportName)
+        {
+            CsvTableReport csvReport = new CsvTableReport(report, reportName);
+            csvReport.Write(csvFileNamePrefix);
+        }
+
+        private void Write(string csvFileNamePrefix)
+        {
+            Report(csvFileNamePrefix + "_alerts.csv", _report.GetAlertRows(TableReport.Row.SortRows_ByName));
+            Report(csvFileNamePrefix + "_areas.csv", _report.GetAreaLabelRows(TableReport.Row.SortRows_ByName));
+            Report(csvFileNamePrefix + "_teams.csv", _report.GetTeamAlertRows(TableReport.Row.SortRows_ByName));
+            Report(csvFileNamePrefix + "_organizations.csv", _report.GetOrganizationAlertRows(TableReport.Row.SortRows_ByName));
+        }
+
+        private void Report(
+            string csvFileName,
+            IEnumerable<TableReport.Row> rows)
+        {
+            using (CsvWriter file = new CsvWriter(csvFileName))
+            {
+                // Prepare last row - "Other (missing above)"
+                Expression otherRowQuery = Expression.And(rows.Select(row => Expression.Not(row.Query)).ToArray());
+                TableReport.Row otherRow = new TableReport.Row(
+                    "Other (missing above)",
+                    otherRowQuery,
+                    null,
+                    _report.Columns,
+                    _report.BeginIssues,
+                    _report.EndIssues);
+
+                // Write heading row
+                file.Write(_reportName ?? "");
+                file.WriteLine(_report.Columns.SelectMany(col => new string[] { col.Name, "new", "gone" }));
+
+                // Write all "middle" rows and last row
+                foreach (TableReport.Row row in rows.Concat(otherRow.ToEnumerable()))
+                {
+                    file.Write(row.Name);
+                    file.WriteLine(row.Columns.SelectMany(filteredIssues => new string[] {
+                            filteredIssues.End.Count().ToString(),
+                            filteredIssues.EndOnly.Count().ToString(),
+                            filteredIssues.BeginOnly.Count().ToString() }));
+                }
+            }
+        }
+    }
+}
---
+++ BugReport/Reports/TableReport.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using BugReport.Util;
+using BugReport.DataModel;
+using BugReport.Query;
+
+namespace BugReport.Reports
+{
+    public class TableReport
+    {
+        private Config _config;
+
+        public readonly IEnumerable<string> BeginFiles;
+        public readonly IEnumerable<string> EndFiles;
+
+        public readonly IEnumerable<DataModelIssue> BeginIssues;
+        public readonly IEnumerable<DataModelIssue> EndIssues;
+
+        public TableReport(IEnumerable<string> configFiles, IEnumerable<string> beginFiles, IEnumerable<string> endFiles)
+        {
+            _config = new Config(configFiles);
+
+            BeginFiles = beginFiles;
+            EndFiles = endFiles;
+
+            IEnumerable<DataModelIssue> beginIssuesAll = IssueCollection.LoadIssues(beginFiles, _config);
+            IEnumerable<DataModelIssue> endIssuesAll = IssueCollection.LoadIssues(endFiles, _config);
+
+            BeginIssues = beginIssuesAll.Where(i => i.IsIssueOrComment).ToArray();
+            EndIssues = endIssuesAll.Where(i => i.IsIssueOrComment).ToArray();
+        }
+
+        public IEnumerable<NamedQuery> Columns
+        {
+            get => _config.Queries;
+        }
+
+        private IEnumerable<Row> GetSortedRows(IEnumerable<NamedQuery> rowQueries, Row.SortRows sortRows)
+        {
+            return sortRows(rowQueries.Select(rowQuery =>
+                new Row(
+                    rowQuery.Name, 
+                    rowQuery.Query, 
+                    rowQuery.Team, 
+                    Columns, 
+                    BeginIssues, 
+                    EndIssues))).ToList();
+        }
+
+        public IEnumerable<Row> GetAlertRows(Row.SortRows sortRows)
+        {
+            return GetSortedRows(_config.Alerts, sortRows);
+        }
+
+        public IEnumerable<Row> GetTeamAlertRows(Row.SortRows sortRows)
+        {
+            return GetSortedRows(
+                _config.Teams.Select(
+                    team =>
+                    new NamedQuery(
+                        team.Name,
+                        Expression.Or(
+                            _config.Alerts.Where(alert => alert.Team == team)
+                                .Select(alert => alert.Query)))),
+                sortRows);
+        }
+
+        public IEnumerable<Row> GetOrganizationAlertRows(Row.SortRows sortRows)
+        {
+            return GetSortedRows(
+                _config.Organizations.Select(
+                    org =>
+                    new NamedQuery(
+                        org.Description,
+                        Expression.Or(
+                            _config.Alerts.Where(alert => alert.IsOrganization(org))
+                                .Select(alert => alert.Query)))),
+                sortRows);
+        }
+
+        private IEnumerable<NamedQuery> _areaLabelQueries = null;
+        public IEnumerable<Row> GetAreaLabelRows(Row.SortRows sortRows)
+        {
+            if (_areaLabelQueries == null)
+            {
+                _areaLabelQueries = _config.AreaLabels.Select(label => label.Name).Distinct()
+                    .Select(labelName => new NamedQuery(labelName, new ExpressionLabel(labelName)))
+                    .ToList();
+            }
+            return GetSortedRows(_areaLabelQueries, sortRows);
+        }
+
+        public class Row : NamedQuery
+        {
+            public FilteredIssues[] Columns { get; private set; }
+
+            public Row(
+                NamedQuery namedQuery,
+                IEnumerable<NamedQuery> columns,
+                IEnumerable<DataModelIssue> beginIssues,
+                IEnumerable<DataModelIssue> endIssues)
+                : base(namedQuery.Name, namedQuery.Query)
+            {
+                InitializeColumns(columns, beginIssues, endIssues);
+            }
+
+            public Row(
+                string name, 
+                Expression query,
+                Team team,
+                IEnumerable<NamedQuery> columns, 
+                IEnumerable<DataModelIssue> beginIssues, 
+                IEnumerable<DataModelIssue> endIssues)
+                : base(name, query, team)
+            {
+                InitializeColumns(columns, beginIssues, endIssues);
+            }
+
+            private void InitializeColumns(
+                IEnumerable<NamedQuery> columns,
+                IEnumerable<DataModelIssue> beginIssues,
+                IEnumerable<DataModelIssue> endIssues)
+            {
+                Columns = columns.Select(col => new FilteredIssues(
+                    Expression.And(Query, col.Query),
+                    beginIssues,
+                    endIssues)).ToArray();
+            }
+
+            public delegate IEnumerable<Row> SortRows(IEnumerable<Row> rows);
+
+            public static IEnumerable<Row> SortRows_ByName(IEnumerable<Row> rows) => rows.OrderBy(row => row.Name);
+            public static IEnumerable<Row> SortRows_ByFirstColumn(IEnumerable<Row> rows) =>
+                rows.OrderByDescending(row => row.Columns[0].End.Count());
+        }
+
+        public class FilteredIssues
+        {
+            public Expression Query { get; private set; }
+            public IEnumerable<DataModelIssue> BeginOnly
+            {
+                get => Begin.Except_ByIssueNumber(End);
+            }
+            public IEnumerable<DataModelIssue> EndOnly
+            {
+                get => End.Except_ByIssueNumber(Begin);
+            }
+            public IEnumerable<DataModelIssue> Begin { get; private set; }
+            public IEnumerable<DataModelIssue> End { get; private set; }
+
+            public FilteredIssues(Expression query, IEnumerable<DataModelIssue> beginIssues, IEnumerable<DataModelIssue> endIssues)
+            {
+                Query = query;
+                Begin = query.Evaluate(beginIssues).ToArray();
+                End = query.Evaluate(endIssues).ToArray();
+            }
+        }
+    }
+}
